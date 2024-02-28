dcfd17d81b3fc972d9a58b7a31d2073442874da5
Inti <intimartinezfloresicube@gmail.com>
2024-02-27 19:41:27 -0600

    Memorama ReactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Board from './components/Board/Board';
+const emojiList = [...'🐎🐖🦓🐤🦖🦕🦎🐊🦆🐧🐋🦭🦈🦀🦑'];
+let attempts = 0;
 
-function App() {
+const App = () => {
+  const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([]);
+  const [selectedMemoBlock, setselectedMemoBlock] = useState(null);
+  const [animating, setAnimating] = useState(false);
+  
+  
+
+  useEffect( () => {
+    const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
+    setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i) => ({ index: i, emoji, flipped: false}) ));
+  }, []);
+
+  const shuffleArray = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  const handleMemoClick = memoBlock => {
+    const flippedMemoBlock = { ...memoBlock, flipped: true };
+    let shuffledMemoBlocksCopy = [...shuffledMemoBlocks];
+    shuffledMemoBlocksCopy.splice(memoBlock.index, 1, flippedMemoBlock);
+    setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+    if(selectedMemoBlock === null) {
+      setselectedMemoBlock(memoBlock);
+    } else if(selectedMemoBlock.emoji === memoBlock.emoji) {
+      setselectedMemoBlock(null);
+    } else {
+      setAnimating(true);
+      setTimeout(() => {
+        shuffledMemoBlocksCopy.splice(memoBlock.index, 1, memoBlock);
+        shuffledMemoBlocksCopy.splice(selectedMemoBlock.index, 1, selectedMemoBlock);
+        setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+        setselectedMemoBlock(null);
+        setAnimating(false);
+      }, 1000);
+      attempts += 1;
+    }
+  }
+  
+  const handleButtonClick = () => {
+    // Lógica a ejecutar al hacer clic en el botón
+    window.location.reload();
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Board memoBlocks={shuffledMemoBlocks} animating={animating} handleMemoClick={handleMemoClick} />
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+          <h1 style={{
+          marginBottom:'10px'
+          }} >  Intentos: {attempts}</h1>
+          <button onClick={handleButtonClick}  style={{
+          backgroundColor: 'black',
+          color: 'white',
+          padding: '10px',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          }}>Reiniciar</button>
+        </div>
+      </div>
   );
+
+
+  
 }
 
 export default App;
